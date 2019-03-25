f2e762b30d17dae4547773a73a945b94ef1cfe3c
Chinh Dinh Luong <44688169+chinhluong99@users.noreply.github.com>
2019-03-25 17:46:01 +0700

    Update tienThuong.js

--- tienThuong/js/tienThuong.js
+++ tienThuong/js/tienThuong.js
@@ -1,12 +1,29 @@
+const testNu = so=>{
+  if(isNaN(parseFloat(so)))
+    return false;
+  else
+    return true;
+}
 const tienThuong = (tien,xepLoai)=>{
-  let heSo = "";
-  if(xepLoai === 'A')
-    heSo = 2;
-  else if(xepLoai === 'B')
-    heSo = 1.8;
-  else if(xepLoai === 'C')
-    heSo = 1.2;
-  else(xepLoai === 'D')
-  heSo = 0.8;
-  return 'Tien thuong la:' + tien*heSo + 'Dong';
+  if(testNu(tien)){
+    if(parseFloat(tien) != 0){
+      tien = parseFloat(tien);
+      let heSo;
+      if(xepLoai == "A")
+        heSo = 2;
+      else if(xepLoai == "B")
+        heSo = 1.8;
+      else if(xepLoai == "C")
+        heSo = 1.2;
+      else
+        heSo = 0.8;
+      return "Tiền thưởng là: " + tien*heSo + " Triệu Đồng";
+    }
+    else
+      return "Nhân viên thực tập không lương";
+  }
+  else{
+    alert('Kí tự bạn nhập không phải số');
+    return 'VUI LÒNG NHẬP LẠI';
+  }
 }
